39b2c3bca2a01badb8f9963a0bd0e589fa8f2fd1
PanFengli <1159929061@qq.com>
2021-06-01 21:21:13 +0800

    feat：基本的UI

---
+++ Paper Folding/Assets/Scripts/FoldPercent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FoldPercent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    private Text text;
+    void Start()
+    {
+        text = GameObject.Find("FoldPercent").GetComponent<Text>();
+        int FoldPercent = (int)slider.value;
+        PlayerPrefs.SetInt("FoldPercent", FoldPercent);
+        text.text = "Fold Percent:" + FoldPercent + "%";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangePercent()
+    {
+        int FoldPercent = (int)slider.value;
+        PlayerPrefs.SetInt("FoldPercent", FoldPercent);
+        text.text = "Fold Percent:" + FoldPercent +"%";
+    }
+}
---
+++ Paper Folding/Assets/Scripts/Reset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Reset : MonoBehaviour
+{
+    private GameObject FoldPercentSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FoldPercentSlider = GameObject.Find("FoldPercentSlider");
+        FoldPercentSlider.GetComponent<Slider>().value = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void reset()
+    {
+        FoldPercentSlider.GetComponent<Slider>().value = 50;
+    }
+}
